4ab889a6a6a19c98ffd3e40f1289e884f19b6122
raisehim <his@acov.kr>
2021-08-18 00:37:14 +0900

    a

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <router-view/>
-    <BottomNavigation/>
+    <router-view />
+    <BottomNavigation />
     <LoadingScreen :isLoading="isLoading" />
     <!-- <div v-if="!isLoading">
     </div> -->
@@ -9,30 +9,31 @@
 </template>
 
 <script>
-import BottomNavigation from './components/layout/BottomNavigation.vue'
-import LoadingScreen from './components/layout/LoadingScreen.vue'
+import BottomNavigation from "./components/layout/BottomNavigation.vue";
+import LoadingScreen from "./components/layout/LoadingScreen.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     BottomNavigation,
-    LoadingScreen
-  }
-}
+    LoadingScreen,
+  },
+};
 </script>
 
 <style>
-html, body {
-  margin:0;
-  padding:0;
-  height:100%;
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
 }
-div{
- margin: 0;
- padding: 0;
+div {
+  margin: 0;
+  padding: 0;
 }
-#app{
-  background-color:blueviolet;
+#app {
+  background-color: blueviolet;
   height: 100%;
   margin: 0;
 }
