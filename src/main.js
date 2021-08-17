3a136ede509a4710688cb8a465705ec8979c3239
raisehim <his@acov.kr>
2021-08-18 00:27:08 +0900

    a

--- src/main.js
+++ src/main.js
@@ -1,21 +1,48 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import $ from 'jquery';
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
 
-Vue.config.productionTip = false
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+const ENV = { isNative: false };
+const loadNativeScripts = async () => new Promise((resolve, reject) => $.getScript('cordova.js', resolve).fail((xhr, s, e) => { reject(e); }));
+
+function onServiceReady() {
+  // Make BootstrapVue available throughout your project
+  Vue.use(BootstrapVue)
+  // Optionally install the BootstrapVue icon components plugin
+  Vue.use(IconsPlugin)
+
+  Vue.config.productionTip = false
+  /* eslint-disable no-new */
+  new Vue({
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
+  });
+}
+
+const onDeviceReady = () => {
+  console.log('Running Cordova' + cordova.platformId + '@' + cordova.version);
+  onServiceReady();
+}
+
+const onDocumentReady = () => {
+  loadNativeScripts().then(
+    () => { // cordova.js file found
+      ENV.isNative = true;
+      document.addEventListener('deviceready', onDeviceReady, false);
+    },
+    (e) => {
+      ENV.isNative = false;
+      console.log('Running Browser');
+      onServiceReady();
+    }
+  );
+}
+
+document.addEventListener('DOMContentLoaded', onDocumentReady, false);
