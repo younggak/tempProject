d8aafff5c76e02192401640d4515905e96e70dff
mmh0705 <bsi05067@gmail.com>
2021-09-08 16:22:39 +0900

    SMS페이지 업데이트

---
+++ platforms/android/app/src/main/java/com/siin/One/SMSBook.java
@@ -0,0 +1,22 @@
+package com.siin.One;
+
+public class SMSBook {
+    private String number;
+    private String text;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
